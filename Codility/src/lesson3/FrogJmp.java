89e3c57ccd108118eb12a74298927a1f26056ac2
hyeongeon <puheh1@gmail.com>
2019-04-16 18:39:19 +0900

    frogjmp

--- Codility/src/lesson3/FrogJmp.java
+++ Codility/src/lesson3/FrogJmp.java
@@ -2,4 +2,15 @@ package lesson3;
 
 public class FrogJmp {
 
+	public int solution(int X, int Y, int D) {
+	
+		int tmp = Y-X;
+		
+		
+		if (tmp % D == 0) {
+			return tmp / D;
+		} else {
+			return tmp/D + 1;
+		}
+	}
 }
